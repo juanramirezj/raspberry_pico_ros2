0c1e239adbe55a55ef2c4099e2eaba6e4f73216e
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2022-06-13 19:02:07 -0400

    oled display and ros corrections

--- motor4_ros/main.c
+++ motor4_ros/main.c
@@ -12,6 +12,10 @@
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
 
+#include "ssd1306.h"  // https://github.com/daschr/pico-ssd1306
+
+#include "motor.h"
+
 #define STRING_BUFFER_LEN 100
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
@@ -19,6 +23,7 @@
 
 
 const uint LED_PIN = 25;
+
 
 rcl_publisher_t publisher;
 rcl_subscription_t ping_subscriber;
@@ -36,6 +41,7 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 
 void ping_subscription_callback(const void * msgin)
 {
+    static int led_status = false;
 	const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
 
 	// Dont pong my own pings
@@ -45,11 +51,63 @@ void ping_subscription_callback(const void * msgin)
 		rcl_publish(&pong_publisher, (const void*)msg, NULL);
 	}
     */
-   printf("Msg received with x=%i,y=%i,z=%i", msg->linear.x, msg->linear.y, msg->linear.z);
+   // printf("Msg received with x=%i,y=%i,z=%i", msg->linear.x, msg->linear.y, msg->linear.z);
+   led_status = !led_status;  
+   if( led_status)
+        gpio_put(LED_PIN, 1);
+    else
+        gpio_put(LED_PIN, 0);
+
+    int speed;
+    int forward;
+
+    speed = msg->linear.x;
+    forward = (speed >= 0);
+
+    BiMotorspeed(&mot1, abs(speed), forward);
+    BiMotorspeed(&mot2, abs(speed), forward); 
+}
+
+void blink_led(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        gpio_put(LED_PIN,1);
+        sleep_ms(250);
+        gpio_put(LED_PIN,0);
+        sleep_ms(250);
+    }
+    sleep_ms(1000);
+}
+
+
+ssd1306_t disp;
+
+void display_init()
+{
+    i2c_init(i2c0, 400000);
+    gpio_set_function(0, GPIO_FUNC_I2C);
+    gpio_set_function(1, GPIO_FUNC_I2C);
+    gpio_pull_up(0);
+    gpio_pull_up(1);
+
+    disp.external_vcc=false;
+    ssd1306_init(&disp, 128, 64, 0x3C, i2c0);
+    ssd1306_clear(&disp);
+}
+
+char debug_txt(char * text)
+{
+            ssd1306_draw_string(&disp, 8, 24, 2, text);
+            ssd1306_show(&disp);
+
 }
 
 int main()
 {
+    display_init();
+    debug_txt("START");
+
     rmw_uros_set_custom_transport(
 		true,
 		NULL,
@@ -61,13 +119,12 @@ int main()
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-
-
+    blink_led(1);
+    
   
     rcl_allocator_t allocator;
     rclc_support_t support;
   
-
     allocator = rcl_get_default_allocator();
 
     // Wait for agent successful ping for 2 minutes.
@@ -76,6 +133,8 @@ int main()
 
     rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
 
+    // blink_led(1);
+    
     if (ret != RCL_RET_OK)
     {
         // Unreachable agent, exiting program.
@@ -84,33 +143,42 @@ int main()
 
     rclc_support_init(&support, 0, NULL, &allocator);
 
+    // blink_led(2);
+
     // Create node
     rcl_node_t node;
     RCCHECK(rclc_node_init_default(&node, "pico_node", "", &support));
-
+    
     // Create a reliable ping publisher
     RCCHECK( rclc_publisher_init_default(&publisher, &node,  ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32) , "pico_publisher"));
 
     // Create a best effort subscriber
-    RCCHECK( rclc_subscription_init_best_effort( &ping_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/pose"))
+    // blink_led(3);
+    RCCHECK( rclc_subscription_init_best_effort( &ping_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/mobile_base_controller/cmd_vel") );
     
     // Create a 1 second ping timer timer
+    // blink_led(4);
     rcl_timer_t timer;
     RCCHECK( rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(1000), timer_callback) );
 
     // Create executor
+    // blink_led(5);
     rclc_executor_t executor;
-    RCCHECK( rclc_executor_init(&executor, &support.context, 1, &allocator) );
+    RCCHECK( rclc_executor_init(&executor, &support.context, 2, &allocator) ); // change the 2 for the number of executors
+
+    // blink_led(6);
     RCCHECK( rclc_executor_add_timer(&executor, &timer) );
-    RCCHECK( rclc_executor_add_subscription(&executor, &ping_subscriber, &incoming_ping, &ping_subscription_callback, ON_NEW_DATA));
 
-    // Create and allocate the messages
+    // blink_led(7);
+    RCCHECK( rclc_executor_add_subscription(&executor, &ping_subscriber, &incoming_ping, &ping_subscription_callback, ON_NEW_DATA) );
 
-	char incoming_ping_buffer[STRING_BUFFER_LEN];
+    // Create and allocate space for the messages
 	
-    gpio_put(LED_PIN, 1);
-
-    msg.data = 0;
+    motor_init_all();
+    
+    test_motor();
+    blink_led(3);    
+    
     while (true)
     {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
---
+++ motor4_ros/motor.h
@@ -0,0 +1,39 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+typedef struct
+{
+    uint gpioForward;
+    uint gpioBackward;
+    uint slice;
+    uint Fchan;
+    uint Bchan;
+    bool forward;
+    uint speed;
+    uint freq;
+    uint resolution;
+    bool on;
+} BiMotor;
+
+extern const uint STBY_PIN;
+
+extern volatile int32_t speed1;
+extern volatile int32_t speed2;
+
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d);
+uint32_t pwm_get_wrap(uint slice_num);
+void pwm_set_duty(uint slice_num, uint chan, int d);
+extern BiMotor mot1;
+extern BiMotor mot2;
+
+void BiMotorInit(BiMotor *m, uint gpioForward, uint gpioBackward, uint freq);
+void BiMotorspeed(BiMotor *m, int s, bool forward);
+void BiMotorOn(BiMotor *m);
+void BiMotorOff(BiMotor *m);
+void MyIRQHandler(uint gpio, uint32_t events);
+bool MyTimerHandler(struct repeating_timer *t);
+int motor_init_all();
+void test_motor();
+
+void blink_led(int n);
+#endif
